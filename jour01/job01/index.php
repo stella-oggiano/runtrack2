44d4163048cd0ef8b5fecbe2f6c1d42e3b10d336
Stella Oggiano <stella.oggiano@laplateforme.io>
2021-11-02 14:39:40 +0100

    job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,17 @@
+<?php
+   $str = "LaPlateforme";
+   $str2 = "Vive " ;
+   $str3 = " !" ;
+
+   echo $str2;
+   echo $str; 
+   echo $str3;
+    
+   $val = 6 + 4;
+   echo $val ;
+
+   $myBool = true ;
+   echo $myBool ;
+   $myBool = false ;
+   echo $myBool ;
+?>
