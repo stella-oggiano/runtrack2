2c37c41078181f26450d62a70c7e3a02e4cfd322
Stella Oggiano <stella.oggiano@laplateforme.io>
2021-11-03 19:37:35 +0100

    job01

---
+++ jour02/job01/index.php
@@ -0,0 +1,27 @@
+
+
+
+
+<?php
+
+$min = 0 ;
+$max = 1337 ;
+
+do {
+     echo  $min++, '</br>' ; 
+    
+} while ($min <= 41 ) ;
+
+
+if ( $min = 42) { echo '<b> <u>',$min++,'</b> </u>' ,'</br>' ;
+
+}
+
+do { echo  $min++, '</br>' ; 
+    
+} while ($min <= $max) ;
+
+
+?>
+
+
