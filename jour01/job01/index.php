fdc0b1dd2f74d6c8edd39a2b8eb01826f99867dd
Stella Oggiano <stella.oggiano@laplateforme.io>
2021-11-03 14:55:59 +0100

    commit

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,12 +1,10 @@
 <?php
    $str = "LaPlateforme";
    $str2 = "Vive " ;
-   $str3 = " !" ;
+   $str3 = " ! " ;
 
-   echo $str2;
-   echo $str; 
-   echo $str3;
-    
+   echo $str2 . $str . $str3 ;
+   
    $val = 6 + 4;
    echo $val ;
 
